a3a347d37c6c35ff20d8fbaa5887e99f239c93b9
McLutzifer <lukasschumi@gmx.at>
2021-01-27 20:52:03 +0100

    finding lowest number

--- littlebookofjulialgorithms/lowest_number.jl
+++ littlebookofjulialgorithms/lowest_number.jl
@@ -1,1 +1,12 @@
 println("Enter the first number: ")
+num1 = parse(Int, readline())
+println("Enter the second number: ")
+num2 = parse(Int, readline())
+
+if num1 <= num2
+    lowest = num1
+else
+    lowest = num2
+end
+
+println("The lowest number is ", lowest)
