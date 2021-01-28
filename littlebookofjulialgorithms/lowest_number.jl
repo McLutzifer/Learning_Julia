97fc1b87e9511429b869d78a783279c284945fbd
McLutzifer <lukasschumi@gmx.at>
2021-01-28 20:12:03 +0100

    try with two parse in terminal

--- littlebookofjulialgorithms/lowest_number.jl
+++ littlebookofjulialgorithms/lowest_number.jl
@@ -2,8 +2,8 @@ println("Enter the first number: ")
 num1 = parse(Int, readline())
 #num1 = 3
 println("Enter the second number: ")
-#num2 = parse(Int, readline())
-num2 = 13
+num2 = parse(Int, readline())
+#num2 = 13
 
 if num1 <= num2
     lowest = num1
