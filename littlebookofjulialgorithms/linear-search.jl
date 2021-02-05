a93bff3324a5821d81adb7a68d250d5a4942902b
McLutzifer <lukasschumi@gmx.at>
2021-02-05 21:15:11 +0100

    Add code

---
+++ littlebookofjulialgorithms/linear-search.jl
@@ -0,0 +1,18 @@
+function linear_search(target)
+    names = ["Elizabeth", "Samuel", "Jawwad", "Yacoub", "Cara", "Jess", "Benji", "Thamber", "Suki", "Zi", "Q"]
+    found = false
+
+    for i in 1:length(names)
+        if target == names[i]
+            println("$(target) found at position $(i)")
+            found = true
+        end
+    end
+    if found == false
+        println("$(target) was not found")
+    end
+end
+
+println("Who are you searching for?")
+name = readline()
+linear_search(name)
