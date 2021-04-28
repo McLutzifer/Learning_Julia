f6067174938c755b506dcd909af0ea997102591b
McLutzifer <lukasschumi@gmx.at>
2021-04-28 17:02:50 +0200

    change datatype in function

--- generic/DerekBanasYoutube.jl
+++ generic/DerekBanasYoutube.jl
@@ -1,6 +1,13 @@
 using Printf
 using Statistics
 
-
-s = "Dog"
+s = 0
+s = "Dog"    # no problem changing variable types
 println(s)
+
+
+##  can't do that ERROR
+function chengeNum(args)
+    x::Int8 = 10
+    x = "Dog"
+end
